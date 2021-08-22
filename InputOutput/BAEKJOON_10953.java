36754685ed731165ec92c11baf7c0e2a8ab48925
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-22 16:24:14 +0900

    change folder structure

--- InputOutput/BAEKJOON_10953.java
+++ InputOutput/BAEKJOON_10953.java
@@ -1,3 +1,5 @@
+package InputOutput;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
--- InputOutput/BAEKJOON_2557.java
+++ InputOutput/BAEKJOON_2557.java
@@ -1,3 +1,5 @@
+package InputOutput;
+
 public class BAEKJOON_2557 {
     public static void main(String[] args){
         System.out.print("Hello World!");
