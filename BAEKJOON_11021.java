7b8524df5d8bcdc87272dea86a65a36bc61c1ab1
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-22 16:13:52 +0900

    resolve BAEKJOON 11021

---
+++ BAEKJOON_11021.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < A; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(("Case #" + (i + 1) + ": " + (a + b)));
+        }
+        br.close();
+    }
+}
