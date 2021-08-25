90593988ce5e574b96fe260032b95c03f0e7fce3
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-26 00:34:37 +0900

    resolve BAEKJOON 2438

---
+++ inputOutput/BAEKJOON_2438.java
@@ -0,0 +1,19 @@
+package inputOutput;
+import java.util.Scanner;
+
+public class BAEKJOON_2438 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        sc.close();
+
+        for (int i = 1; i < num + 1; i++){
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
