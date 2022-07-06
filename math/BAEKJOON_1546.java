0a323ea5a2a7b8e13351cddbb10fac585b86fb2d
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-07 02:18:55 +0900

    resolve BAEKJOON_1546

---
+++ math/BAEKJOON_1546.java
@@ -0,0 +1,31 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int max = -1;
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if(value > max) {
+                max = value;
+            }
+
+            sum += value;
+        }
+
+        System.out.println(((sum / max) * 100) / N);
+    }
+}
