4a4153d3a23f75aad20a2c542f18f6a9cdd90a8b
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-07 01:59:53 +0900

    resolve BAEKJOON 1008

---
+++ inputOutput/BAEKJOON_1008.java
@@ -0,0 +1,20 @@
+package inputOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_1008 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a / b);
+    }
+}
