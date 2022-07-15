c5149bc1acdc06517a1522708ce2ef2e57882734
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-16 04:08:51 +0900

    resolve Programmers 최소직사각형

---
+++ bruteForce/Programmers_최소직사각형.java
@@ -0,0 +1,16 @@
+package bruteForce;
+
+public class Programmers_최소직사각형 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxW = 0;
+        int maxH = 0;
+
+        for(int i = 0;i<sizes.length;i++){
+            maxW = Math.max(maxW, Math.max(sizes[i][0],sizes[i][1]));
+            maxH = Math.max(maxH, Math.min(sizes[i][0],sizes[i][1]));
+        }
+        answer = maxW * maxH;
+        return answer;
+    }
+}
