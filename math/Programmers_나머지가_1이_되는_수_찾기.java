d4d3d9bc908e1829cca3c0564cb7416d38badef2
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-15 21:04:22 +0900

    resolve Programmers 나머지가 1이 되는 수 찾기

---
+++ math/Programmers_나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,14 @@
+package math;
+
+class Solution {
+    public int solution(int n) {
+        int x = 0;
+        while (true) {
+            x++;
+            if (n % x == 1) {
+                break;
+            }
+        }
+        return x;
+    }
+}
