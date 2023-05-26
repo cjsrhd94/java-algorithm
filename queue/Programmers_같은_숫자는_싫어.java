a5f3df4368999c2c9964495f81fd919b8ffa41d5
cjsrhd94 <cjsrhd94@gmail.com>
2023-05-26 12:40:53 +0900

    fix: remove main class in Programmers 같은 숫자는 싫어

--- queue/Programmers_같은_숫자는_싫어.java
+++ queue/Programmers_같은_숫자는_싫어.java
@@ -1,14 +1,8 @@
 package queue;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Programmers_같은_숫자는_싫어 {
-	public static void main(String[] args) throws IOException {
-		Programmers_같은_숫자는_싫어 main = new Programmers_같은_숫자는_싫어();
-		int[] arr = {1,2,3,3,2,1};
-		System.out.println(main.solution(arr));
-	}
 	public int[] solution(int []arr) {
 		ArrayList<Integer> answerList = new ArrayList<>();
 
